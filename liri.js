d11fe742a57fb5a0bb3abb2fd9ff3f0775ac946a
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-15 00:21:01 -0500

    cleaned up code, added processCommands()

--- liri.js
+++ liri.js
@@ -4,32 +4,37 @@ require("dotenv").config();
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
-var userInput = process.argv[3];
-var userCommand = process.argv[2];
+var userInputVar = process.argv[3];
+var userCommandVar = process.argv[2];
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//liri.js commands:
-switch (userCommand) {
-  case "my-tweets":
-    myTweets();
-    break;
 
-  case "spotify-this-song":
-    spotifyThisSong();
-    break;
+function processCommands(userCommand, userInput) {
+  switch (userCommand) {
+    case "my-tweets":
+      myTweets(userInput);
+      break;
 
-  case "movie-this":
-    movieThis();
-    break;
+    case "spotify-this-song":
+      spotifyThisSong(userInput);
+      break;
 
-  case "do-what-it-says":
-    doWhatItSays();
-    break;
+    case "movie-this":
+      movieThis(userInput);
+      break;
 
-  case "help":
-    help();
-}
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+
+    case "help":
+      help();
+      break;
+  }
+};
+
+processCommands(userCommandVar, userInputVar);
 
 function help() {
   console.log("\nStart by typing 'node liri.js' followed by one of the following commands: " +
@@ -39,8 +44,7 @@ function help() {
 
 //=========================================
 
-function myTweets() {
-  var params = process.argv[3];
+function myTweets(params) {
   if (!params) {
     params = { screen_name: "bootcamp_ut" };
   };
@@ -56,8 +60,7 @@ function myTweets() {
 
 //==========================================
 
-function spotifyThisSong() {
-  var songName = process.argv[3];
+function spotifyThisSong(songName) {
   if (!songName) {
     songName = "the sign artist:ace of base";
   }
@@ -68,8 +71,7 @@ function spotifyThisSong() {
     else {
       var firstItem = data.tracks.items[0];
       if (firstItem) {
-        // console.log(firstItem.artists.name);
-        console.log("Artist: " + firstItem.artists[0].name);
+        console.log("\nArtist: " + firstItem.artists[0].name);
         console.log("\nSong: " + firstItem.name);
         console.log("\nPreview URL: " + firstItem.preview_url);
         console.log("\nAlbum: " + firstItem.album.name);
@@ -81,7 +83,7 @@ function spotifyThisSong() {
 //==========================================
 
 function movieInfo(body1) {
-  console.log("Title: " + JSON.parse(body1).Title);
+  console.log("\nTitle: " + JSON.parse(body1).Title);
   console.log("Year the movie came out: " + JSON.parse(body1).Year);
   console.log("IMDB Rating: " + JSON.parse(body1).imdbRating);
   console.log("Rotten Tomatoes Rating: " + JSON.parse(body1).Ratings[1].Value);
@@ -90,12 +92,11 @@ function movieInfo(body1) {
   console.log("Plot: " + JSON.parse(body1).Plot);
   console.log("Actors: " + JSON.parse(body1).Actors);
 };
-function movieThis() {
-  if (!userInput) {
-    userInput = "mr.nobody";
+function movieThis(movieName) {
+  if (!movieName) {
+    movieName = "mr.nobody";
   };
-  var queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-  // console.log(queryURL);
+  var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
   request(queryURL, function (error, response, body) {
     if (error) {
@@ -109,22 +110,16 @@ function movieThis() {
 };
 
 //===============================================
-// do-what-it-says
 
-function doWhatItSays(){
-fs.readFile("random.txt", "utf8", function(error, data) {
-  if (error) {
-    return console.log(error);
-  }
-  var dataArr = data.split(",");
-  console.log("$ node liri.js " + dataArr[0] + " " + dataArr[1]);
-});
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    var dataArr = data.split(",");
+    processCommands(dataArr[0], dataArr[1]);
+  });
 };
-
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-
 
 //===============================================
 
